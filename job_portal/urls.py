f4e8304188c6892427a9fbe5c5aeca52c373e015
Dinesh Karthik <dineshkarthik0218@gmail.com>
2026-03-18 14:29:47 +0530

    fixed add job url

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -5,6 +5,7 @@ from jobs.views import job_list, recruiter_jobs
 from applications.views import apply_job, job_applications
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import add_job
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,15 +14,16 @@ urlpatterns = [
     path('register/', register_page),
     path('login/', login_page),
     path('logout/', logout_page),
+
     path('apply/<int:job_id>/', apply_job),
+
+    path('add-job/', add_job),  # ✅ IMPORTANT
+    path('my-jobs/', recruiter_jobs),
+    path('job-applications/<int:job_id>/', job_applications),
 
     path('api/', include('accounts.urls')),
     path('api/', include('jobs.urls')),
     path('api/', include('applications.urls')),
-
-    path('my-jobs/', recruiter_jobs),
-    path('job-applications/<int:job_id>/', job_applications),
 ]
 
-# ✅ ALWAYS AT BOTTOM
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
